08cca000430b1064de1fceb31cef6916d5a4f5d5
aspklman <562142139@qq.com>
2019-08-05 08:45:41 +0800

    修改订单9

---
+++ src/views/bom/StylemList.vue
@@ -0,0 +1,357 @@
+<template>
+  <a-card :bordered="false">
+
+    <!-- 查询区域 -->
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline">
+        <a-row :gutter="24">
+<!--          <a-col :span="6">-->
+<!--            <a-form-item label="厂区编号">-->
+<!--              <a-input placeholder="请输入厂区编号" v-model="queryParam.factNo"></a-input>-->
+<!--            </a-form-item>-->
+<!--          </a-col>-->
+          <a-col :span="6">
+            <a-form-item label="型体编号">
+              <a-input placeholder="请输入型体编号" v-model="queryParam.styleNo"></a-input>
+            </a-form-item>
+          </a-col>
+          <a-col :span="8" >
+            <span style="float: left;overflow: hidden;" class="table-page-search-submitButtons">
+              <a-button type="primary" @click="searchQuery" icon="search">查询</a-button>
+              <a-button type="primary" @click="searchReset" icon="reload" style="margin-left: 8px">重置</a-button>
+            </span>
+          </a-col>
+
+        </a-row>
+      </a-form>
+    </div>
+
+    <!-- 操作按钮区域 -->
+    <div class="table-operator">
+      <a-button @click="handleAdd" type="primary" icon="plus">新增</a-button>
+      <a-button type="primary" icon="download" @click="handleExportXls('鞋型基本资料表')">导出</a-button>
+      <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" @change="handleImportExcel">
+        <a-button type="primary" icon="import">导入</a-button>
+      </a-upload>
+      <a-dropdown v-if="selectedRowKeys.length > 0">
+        <a-menu slot="overlay">
+          <a-menu-item key="1" @click="batchDel"><a-icon type="delete"/>删除</a-menu-item>
+        </a-menu>
+        <a-button style="margin-left: 8px"> 批量操作 <a-icon type="down" /></a-button>
+      </a-dropdown>
+    </div>
+
+    <!-- table区域-begin -->
+    <div>
+      <div class="ant-alert ant-alert-info" style="margin-bottom: 16px;">
+        <i class="anticon anticon-info-circle ant-alert-icon"></i>
+        <span>已选择</span>
+        <a style="font-weight: 600">
+          {{ selectedRowKeys.length }}
+        </a>
+        <span>项</span>
+        <a style="margin-left: 24px" @click="onClearSelected">清空</a>
+      </div>
+
+      <a-table
+        ref="table"
+        size="middle"
+        bordered
+        rowKey="id"
+        :columns="columns"
+        :dataSource="dataSource"
+        :pagination="ipagination"
+        :loading="loading"
+        :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+        @change="handleTableChange">
+
+        <span slot="action" slot-scope="text, record">
+          <a @click="handleEdit(record)">编辑</a>
+
+          <a-divider type="vertical" />
+          <a-dropdown>
+            <a class="ant-dropdown-link">更多 <a-icon type="down" /></a>
+            <a-menu slot="overlay">
+              <a-menu-item>
+                <a-popconfirm title="确定删除吗?" @confirm="() => handleDelete(record.id)">
+                  <a>删除</a>
+                </a-popconfirm>
+              </a-menu-item>
+            </a-menu>
+          </a-dropdown>
+        </span>
+
+      </a-table>
+    </div>
+    <!-- table区域-end -->
+
+    <!-- 表单区域 -->
+    <stylem-modal ref="modalForm" @ok="modalFormOk"/>
+
+  </a-card>
+</template>
+
+<script>
+
+  import { JeecgListMixin } from '@/mixins/JeecgListMixin'
+  import StylemModal from './modules/StylemModal'
+
+  export default {
+    name: "StylemList",
+    mixins: [JeecgListMixin],
+    components: {
+      StylemModal
+    },
+    data () {
+      return {
+        description: '鞋型基本资料表管理页面',
+        // 表头
+        columns: [
+          {
+            title: '#',
+            dataIndex: '',
+            key: 'rowIndex',
+            width: 60,
+            align: "center",
+            customRender:function (t, r, index) {
+              return parseInt(index)+1;
+            }
+          },
+          // {
+          //   title: '厂区编号',
+          //   align:"center",
+          //   dataIndex: 'factNo'
+          // },
+          {
+            title: '品牌编号',
+            align:"center",
+            dataIndex: 'brandNo'
+          },
+          {
+            title: '型体编号',
+            align:"center",
+            dataIndex: 'styleNo'
+          },
+          {
+            title: '客户型体编号',
+            align:"center",
+            dataIndex: 'articNo'
+          },
+          // {
+          //   title: '型体名称',
+          //   align:"center",
+          //   dataIndex: 'styleNm'
+          // },
+          // {
+          //   title: '财务型体简称,财务成本中心维护',
+          //   align:"center",
+          //   dataIndex: 'styleShorten'
+          // },
+          // {
+          //   title: '底模编号',
+          //   align:"center",
+          //   dataIndex: 'moldNo'
+          // },
+          // {
+          //   title: '超始BOM确认日',
+          //   align:"center",
+          //   dataIndex: 'bomStcfmDate'
+          // },
+          // {
+          //   title: '起始用量确认日',
+          //   align:"center",
+          //   dataIndex: 'stdqStcfmDate'
+          // },
+          // {
+          //   title: '性别',
+          //   align:"center",
+          //   dataIndex: 'gender'
+          // },
+          // {
+          //   title: '楦头编号',
+          //   align:"center",
+          //   dataIndex: 'lastNo'
+          // },
+          // {
+          //   title: '英文颜色',
+          //   align:"center",
+          //   dataIndex: 'styleEngColor'
+          // },
+          // {
+          //   title: '型体颜色',
+          //   align:"center",
+          //   dataIndex: 'styleColor'
+          // },
+          // {
+          //   title: '型体工艺类别',
+          //   align:"center",
+          //   dataIndex: 'solColor'
+          // },
+          // {
+          //   title: '商品编号',
+          //   align:"center",
+          //   dataIndex: 'goodsNo'
+          // },
+          // {
+          //   title: '序号',
+          //   align:"center",
+          //   dataIndex: 'goodsSeq'
+          // },
+          // {
+          //   title: '鞋型类别编号',
+          //   align:"center",
+          //   dataIndex: 'styletypeNo'
+          // },
+          // {
+          //   title: '异动人',
+          //   align:"center",
+          //   dataIndex: 'userNo'
+          // },
+          // {
+          //   title: '异动时间',
+          //   align:"center",
+          //   dataIndex: 'modifyDt'
+          // },
+          // {
+          //   title: '型体单价',
+          //   align:"center",
+          //   dataIndex: 'price'
+          // },
+          // {
+          //   title: '通用型体名称',
+          //   align:"center",
+          //   dataIndex: 'univModelName'
+          // },
+          // {
+          //   title: '报价SIZE编号',
+          //   align:"center",
+          //   dataIndex: 'quotationSizeNo'
+          // },
+          {
+            title: '报价楦头肥度',
+            align:"center",
+            dataIndex: 'quotationLastWidth'
+          },
+          // {
+          //   title: '纸版编号',
+          //   align:"center",
+          //   dataIndex: 'paperNo'
+          // },
+          // {
+          //   title: '客户型体代码',
+          //   align:"center",
+          //   dataIndex: 'vampPartNo'
+          // },
+          // {
+          //   title: '客户颜色代码',
+          //   align:"center",
+          //   dataIndex: 'cusColorNo'
+          // },
+          // {
+          //   title: '来源型体',
+          //   align:"center",
+          //   dataIndex: 'sourNo'
+          // },
+          // {
+          //   title: '转印纸注记',
+          //   align:"center",
+          //   dataIndex: 'htpRmk'
+          // },
+          // {
+          //   title: '转印纸用量',
+          //   align:"center",
+          //   dataIndex: 'htpQty'
+          // },
+          // {
+          //   title: '取消注记',
+          //   align:"center",
+          //   dataIndex: 'cnlRmk'
+          // },
+          // {
+          //   title: '取消日期',
+          //   align:"center",
+          //   dataIndex: 'cnlDate'
+          // },
+          // {
+          //   title: 'CUSTRAN_RMK',
+          //   align:"center",
+          //   dataIndex: 'custranRmk'
+          // },
+          // {
+          //   title: 'CUSTRAN_DT',
+          //   align:"center",
+          //   dataIndex: 'custranDt'
+          // },
+          // {
+          //   title: 'ARTIC_NM',
+          //   align:"center",
+          //   dataIndex: 'articNm'
+          // },
+          // {
+          //   title: 'BOM审核人',
+          //   align:"center",
+          //   dataIndex: 'bomVerifyUser'
+          // },
+          // {
+          //   title: 'BOM审核时间',
+          //   align:"center",
+          //   dataIndex: 'bomVerifyDt'
+          // },
+          // {
+          //   title: '型体单位',
+          //   align:"center",
+          //   dataIndex: 'styleUnit'
+          // },
+          // {
+          //   title: 'soletypeNo',
+          //   align:"center",
+          //   dataIndex: 'soletypeNo'
+          // },
+          {
+            title: '创建时间',
+            align:"center",
+            dataIndex: 'createTime',
+            sorter: true,
+            width: 160
+          },
+          {
+            title: '修改时间',
+            align:"center",
+            dataIndex: 'updateTime',
+            sorter: true
+          },
+          {
+            title: '操作',
+            dataIndex: 'action',
+            align:"center",
+            scopedSlots: { customRender: 'action' },
+          }
+        ],
+        // 请求参数
+    	url: {
+              list: "/bom/stylem/list",
+              delete: "/bom/stylem/delete",
+              deleteBatch: "/bom/stylem/deleteBatch",
+              exportXlsUrl: "bom/stylem/exportXls",
+              importExcelUrl: "bom/stylem/importExcel",
+           },
+        }
+      },
+      computed: {
+        importExcelUrl: function(){
+          return `${window._CONFIG['domianURL']}/${this.url.importExcelUrl}`;
+        }
+      },
+
+
+    methods: {
+
+      initDictConfig() {
+      }
+
+    }
+  }
+</script>
+<style scoped>
+  @import '~@assets/less/common.less'
+</style>
---
+++ src/views/bom/modules/StylemModal.vue
@@ -0,0 +1,487 @@
+<template>
+  <a-modal
+    :title="title"
+    :width="1200"
+    :visible="visible"
+    :maskClosable="false"
+    :confirmLoading="confirmLoading"
+    @ok="handleOk"
+    @cancel="handleCancel">
+    <a-spin :spinning="confirmLoading">
+      <!-- 主表单区域 -->
+      <a-form :form="form">
+        <a-row>
+<!--          <a-col :span="12" :gutter="8">-->
+<!--            <a-form-item-->
+<!--              :labelCol="labelCol"-->
+<!--              :wrapperCol="wrapperCol"-->
+<!--              label="厂区编号">-->
+<!--              <a-input placeholder="请输入厂区编号" v-decorator="['factNo', validatorRules.factNo ]"/>-->
+<!--            </a-form-item>-->
+<!--          </a-col>-->
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="型体编号">
+              <a-input placeholder="请输入型体编号" v-decorator="['styleNo', validatorRules.styleNo ]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="客户型体编号">
+              <a-input placeholder="请输入客户型体编号" v-decorator="['articNo', validatorRules.articNo ]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="型体名称">
+              <a-input placeholder="请输入型体名称" v-decorator="['styleNm', validatorRules.styleNm ]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="财务型体简称,财务成本中心维护">
+              <a-input placeholder="请输入财务型体简称,财务成本中心维护" v-decorator="['styleShorten', {}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="底模编号">
+              <a-input placeholder="请输入底模编号" v-decorator="['moldNo', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="超始BOM确认日">
+              <a-input placeholder="请输入超始BOM确认日" v-decorator="['bomStcfmDate', {}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="起始用量确认日">
+              <a-input placeholder="请输入起始用量确认日" v-decorator="['stdqStcfmDate', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="性别">
+              <a-input placeholder="请输入性别" v-decorator="['gender', validatorRules.gender ]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="楦头编号">
+              <a-input placeholder="请输入楦头编号" v-decorator="['lastNo', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="英文颜色">
+              <a-input placeholder="请输入英文颜色" v-decorator="['styleEngColor', {}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="型体颜色">
+              <a-input placeholder="请输入型体颜色" v-decorator="['styleColor', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="型体工艺类别">
+              <a-input placeholder="请输入型体工艺类别" v-decorator="['solColor', {}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="商品编号">
+              <a-input placeholder="请输入商品编号" v-decorator="['goodsNo', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="序号">
+              <a-input placeholder="请输入序号" v-decorator="['goodsSeq', {}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="品牌编号">
+              <a-input placeholder="请输入品牌编号" v-decorator="['brandNo', validatorRules.brandNo ]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="鞋型类别编号">
+              <a-input placeholder="请输入鞋型类别编号" v-decorator="['styletypeNo', validatorRules.styletypeNo ]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="异动人">
+              <a-input placeholder="请输入异动人" v-decorator="['userNo', validatorRules.userNo ]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="异动时间">
+              <a-input placeholder="请输入异动时间" v-decorator="['modifyDt', validatorRules.modifyDt ]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="型体单价">
+              <a-input placeholder="请输入型体单价" v-decorator="['price', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="通用型体名称">
+              <a-input placeholder="请输入通用型体名称" v-decorator="['univModelName', validatorRules.univModelName ]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="报价SIZE编号">
+              <a-input placeholder="请输入报价SIZE编号" v-decorator="['quotationSizeNo', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="报价楦头肥度">
+              <a-input placeholder="请输入报价楦头肥度" v-decorator="['quotationLastWidth', {}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="纸版编号">
+              <a-input placeholder="请输入纸版编号" v-decorator="['paperNo', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="客户型体代码">
+              <a-input placeholder="请输入客户型体代码" v-decorator="['vampPartNo', {}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="客户颜色代码">
+              <a-input placeholder="请输入客户颜色代码" v-decorator="['cusColorNo', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="来源型体">
+              <a-input placeholder="请输入来源型体" v-decorator="['sourNo', {}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="转印纸注记">
+              <a-input placeholder="请输入转印纸注记" v-decorator="['htpRmk', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="转印纸用量">
+              <a-input placeholder="请输入转印纸用量" v-decorator="['htpQty', {}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="取消注记">
+              <a-input placeholder="请输入取消注记" v-decorator="['cnlRmk', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="取消日期">
+              <a-input placeholder="请输入取消日期" v-decorator="['cnlDate', {}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="CUSTRAN_RMK">
+              <a-input placeholder="请输入CUSTRAN_RMK" v-decorator="['custranRmk', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="CUSTRAN_DT">
+              <a-input placeholder="请输入CUSTRAN_DT" v-decorator="['custranDt', {}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="ARTIC_NM">
+              <a-input placeholder="请输入ARTIC_NM" v-decorator="['articNm', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="BOM审核人">
+              <a-input placeholder="请输入BOM审核人" v-decorator="['bomVerifyUser', {}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="BOM审核时间">
+              <a-input placeholder="请输入BOM审核时间" v-decorator="['bomVerifyDt', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="型体单位">
+              <a-input placeholder="请输入型体单位" v-decorator="['styleUnit', {}]"/>
+            </a-form-item>
+          </a-col>
+          <a-col :span="12" :gutter="8">
+            <a-form-item
+              :labelCol="labelCol"
+              :wrapperCol="wrapperCol"
+              label="soletypeNo">
+              <a-input placeholder="请输入soletypeNo" v-decorator="['soletypeNo', {}]"/>
+            </a-form-item>
+          </a-col>
+        </a-row>
+      </a-form>
+
+      <!-- 子表单区域 -->
+      <a-tabs v-model="activeKey" @change="handleChangeTabs">
+        <a-tab-pane tab="鞋型SIZE表" :key="refKeys[0]" :forceRender="true">
+          <j-editable-table
+            :ref="refKeys[0]"
+            :loading="stylesTable.loading"
+            :columns="stylesTable.columns"
+            :dataSource="stylesTable.dataSource"
+            :maxHeight="300"
+            :rowNumber="true"
+            :rowSelection="true"
+            :actionButton="true"/>
+        </a-tab-pane>
+      </a-tabs>
+
+    </a-spin>
+  </a-modal>
+</template>
+
+<script>
+
+  import moment from 'moment'
+  import pick from 'lodash.pick'
+  import { FormTypes } from '@/utils/JEditableTableUtil'
+  import { JEditableTableMixin } from '@/mixins/JEditableTableMixin'
+
+  export default {
+    name: 'StylemModal',
+    mixins: [JEditableTableMixin],
+    data() {
+      return {
+        // 新增时子表默认添加几行空数据
+        addDefaultRowNum: 1,
+        validatorRules: {
+          factNo: { rules: [{ required: true, message: '请输入厂区编号!' }] },
+          styleNo: { rules: [{ required: true, message: '请输入型体编号!' }] },
+          articNo: { rules: [{ required: true, message: '请输入客户型体编号!' }] },
+          styleNm: { rules: [{ required: true, message: '请输入型体名称!' }] },
+          gender: { rules: [{ required: true, message: '请输入性别!' }] },
+          brandNo: { rules: [{ required: true, message: '请输入品牌编号!' }] },
+          styletypeNo: { rules: [{ required: true, message: '请输入鞋型类别编号!' }] },
+          userNo: { rules: [{ required: true, message: '请输入异动人!' }] },
+          modifyDt: { rules: [{ required: true, message: '请输入异动时间!' }] },
+          univModelName: { rules: [{ required: true, message: '请输入通用型体名称!' }] },
+        },
+        refKeys: ['styles', ],
+        activeKey: 'styles',
+        // 鞋型SIZE表
+        stylesTable: {
+          loading: false,
+          dataSource: [],
+          columns: [
+            // {
+            //   title: '厂区编号',
+            //   key: 'factNo',
+            //   type: FormTypes.input,
+            //   defaultValue: '',
+            //   placeholder: '请输入${title}',
+            //   validateRules: [{ required: true, message: '${title}不能为空' }],
+            // },
+            {
+              title: '型体编号',
+              key: 'styleNo',
+              type: FormTypes.input,
+              defaultValue: '',
+              placeholder: '请输入${title}',
+              validateRules: [{ required: true, message: '${title}不能为空' }],
+            },
+            {
+              title: 'SIZE编号',
+              key: 'sizeNo',
+              type: FormTypes.input,
+              defaultValue: '',
+              placeholder: '请输入${title}',
+              validateRules: [{ required: true, message: '${title}不能为空' }],
+            },
+            {
+              title: 'SKU_NO',
+              key: 'skuNo',
+              type: FormTypes.input,
+              defaultValue: '',
+              placeholder: '请输入${title}',
+            },
+            {
+              title: '鞋型基本资料表ID',
+              key: 'styleId',
+              type: FormTypes.input,
+              defaultValue: '',
+              placeholder: '请输入${title}',
+            },
+          ]
+        },
+        url: {
+          add: "/bom/stylem/add",
+          edit: "/bom/stylem/edit",
+          styles: {
+            list: '/bom/stylem/queryStylesByMainId'
+          },
+        }
+      }
+    },
+    methods: {
+ 
+      /** 调用完edit()方法之后会自动调用此方法 */
+      editAfter() {
+        this.$nextTick(() => {
+          this.form.setFieldsValue(pick(this.model, 'factNo', 'styleNo', 'articNo', 'styleNm', 'styleShorten', 'moldNo', 'bomStcfmDate', 'stdqStcfmDate', 'gender', 'lastNo', 'styleEngColor', 'styleColor', 'solColor', 'goodsNo', 'goodsSeq', 'brandNo', 'styletypeNo', 'userNo', 'modifyDt', 'price', 'univModelName', 'quotationSizeNo', 'quotationLastWidth', 'paperNo', 'vampPartNo', 'cusColorNo', 'sourNo', 'htpRmk', 'htpQty', 'cnlRmk', 'cnlDate', 'custranRmk', 'custranDt', 'articNm', 'bomVerifyUser', 'bomVerifyDt', 'styleUnit', 'soletypeNo', ))
+          // 时间格式化
+        })
+        // 加载子表数据
+        if (this.model.id) {
+          let params = { id: this.model.id }
+          this.requestSubTableData(this.url.styles.list, params, this.stylesTable)
+        }
+      },
+ 
+      /** 整理成formData */
+      classifyIntoFormData(allValues) {
+        let main = Object.assign(this.model, allValues.formValue)
+        //时间格式化
+        return {
+          ...main, // 展开
+          stylesList: allValues.tablesValue[0].values,
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
